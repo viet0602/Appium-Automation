2e4add6361b3914f4b7aa65abf57a3f771a6aab5
viet0602 <viet0602@gmail.com>
2022-05-23 00:02:30 +0700

    Update lession 02

--- src/lession_02/Lab_22.java
+++ src/lession_02/Lab_22.java
@@ -8,7 +8,7 @@ public class Lab_22 {
         System.out.print("Please input your number: ");
         int number = sc.nextInt();
         if (number % 2 == 0) {
-            System.out.println("Your inputted number is even number");
+            System.out.println("Your input number is even number");
         } else {
             System.out.println("Your input number is odd number");
         }
